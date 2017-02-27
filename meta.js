f7c3c8594bdd9581ca1bdf9e2c54302d2417b4e3
Wenris Chen <wenris@yeah.net>
2017-02-27 17:14:37 +0800

    remove unit & e2e tests option

--- meta.js
+++ meta.js
@@ -73,23 +73,24 @@ module.exports = {
           "short": "none"
         }
       ]
-    },
-    "unit": {
-      "type": "confirm",
-      "message": "Setup unit tests with Karma + Mocha?"
-    },
-    "e2e": {
-      "type": "confirm",
-      "message": "Setup e2e tests with Nightwatch?"
     }
+    // ,
+    // "unit": {
+    //   "type": "confirm",
+    //   "message": "Setup unit tests with Karma + Mocha?"
+    // },
+    // "e2e": {
+    //   "type": "confirm",
+    //   "message": "Setup e2e tests with Nightwatch?"
+    // }
   },
   "filters": {
     ".eslintrc.js": "lint",
     ".eslintignore": "lint",
-    "config/test.env.js": "unit || e2e",
-    "test/unit/**/*": "unit",
-    "build/webpack.test.conf.js": "unit",
-    "test/e2e/**/*": "e2e",
+    // "config/test.env.js": "unit || e2e",
+    // "test/unit/**/*": "unit",
+    // "build/webpack.test.conf.js": "unit",
+    // "test/e2e/**/*": "e2e",
     "src/router/**/*": "router"
   },
   "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuel.github.io/webpack"
